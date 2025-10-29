9386d82d945371cde348078b43087c29d5d3b47d
brilliahib <brilliahib21@gmail.com>
2025-10-29 12:49:03 +0700

    chore: setup common and infra for folder structure

---
+++ prisma/migrations/20251029054550_added_users/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fullname` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `phone_number` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'CUSTOMER', 'PARTNER') NOT NULL DEFAULT 'CUSTOMER',
+    `is_verified` BOOLEAN NOT NULL DEFAULT false,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_phone_number_key`(`phone_number`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
